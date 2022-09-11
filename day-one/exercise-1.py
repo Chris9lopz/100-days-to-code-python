6789dcbfecaa08ac100f22ed74c1358dadffdd1d
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-09-10 19:24:57 -0500

    Creacion de repositorio, carpeta dia uno y creacion de ejercicio 1 del bootcamp de python

---
+++ day-one/exercise-1.py
@@ -0,0 +1,12 @@
+# Escribir un programa que tenga como salida este output
+
+"""
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+
+"""
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
