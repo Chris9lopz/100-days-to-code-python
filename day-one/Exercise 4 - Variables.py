8032e37abfe3616c0340c64ab968555cffaad6cb
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-09-10 21:15:58 -0500

    Se realiza el ejercicio numero 4 del dia uno del bootcamp

---
+++ day-one/Exercise 4 - Variables.py
@@ -0,0 +1,21 @@
+# Almacenar en memoria dos variables indicadas por el usuario
+# Imprimir que almaceno las variables alternando que almaceno cada una
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a1 = a
+b1 = b
+a = b1
+b = a1
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
