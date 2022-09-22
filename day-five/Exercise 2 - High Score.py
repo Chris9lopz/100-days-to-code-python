399e2670706f84fa1420e9c233135414d03b2c6b
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-09-22 10:55:54 -0500

    Se realiza el ejercicio 2 del día 5 se hace la evaluacion de los inputs con score de 100%

---
+++ day-five/Exercise 2 - High Score.py
@@ -0,0 +1,20 @@
+# Instructions
+"""
+You are going to write a program that calculates the highest score from a List of scores.
+
+Important you are not allowed to use the max or min functions. The output words must match the example.
+"""
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+num_max = 0
+for score in student_scores:
+    if score > num_max:
+        num_max = score
+print(f'The highest score in the class is: {num_max}')
