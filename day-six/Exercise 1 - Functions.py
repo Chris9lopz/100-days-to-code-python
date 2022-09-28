b4a55ef4db43d26282258530cb694d85f996464b
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-09-27 21:28:47 -0500

    Se agregan archivos dia 6, contiene introduccion a las funciones y carrera de obstaculos en página reborg

---
+++ day-six/Exercise 1 - Functions.py
@@ -0,0 +1,20 @@
+# Create a function that allow to the robot turn to the left
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+# move() is already defined, try to do a 360° move through reeborg program
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
