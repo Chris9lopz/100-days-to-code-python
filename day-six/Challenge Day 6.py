3d2a19a18d6c42a4d92f505340e45c9a2e508f63
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-09-30 14:13:06 -0500

    Se realiza desafío dia 6 de laberinto, se realiza através de condicionales y loop while

---
+++ day-six/Challenge Day 6.py
@@ -0,0 +1,22 @@
+# Maze
+
+# Create an option that allows to Reborn to get out of the maze
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    elif wall_in_front() and wall_on_right():
+        turn_left()
+    elif wall_in_front() and not wall_on_right():
+        turn_right()
