ce44bbb7a4a67fbe98480de7f8cb4cb484930fbc
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-10-19 22:42:18 -0500

    Se agregan comentarios y while loop que pregunte al usuario hasta que se complete la palabra

--- day-seven/hangman.py
+++ day-seven/hangman.py
@@ -1,42 +1,39 @@
 import random
 
-# Create a hangman game 
+# Create a hangman game
 
-# Step 1: Create a list of random words
-
+# Create a list of posible words
 word_list = ["jirafa","elefante","tigre","leon","pulpo","tiburon","dinosaurio"]
 
-# Step 2: Choose randomly a word into the list
-
+# Select a random word from the list
 chosen_word = random.choice(word_list)
+
 print(f'This is a clue {chosen_word}')
 
-# Step 3: Ask to the user a letter
-
-guess = input("Guess a letter: ").lower()
-
-# Step 4: Validate if the letter is into the chosen_word 
-
-for letter in chosen_word:
-    if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
-
-# Step 5: Create a blank list and replace the spaces '_' with the correct letter
-
+# Create a empty list 
 blank_list = []
+
+# Add into the empty list the length of the chosen word
 for i in range(len(chosen_word)):
-  blank_list.append("_")
+    blank_list.append("_")
 
-print(blank_list)
+# Loop until is not '_' more in the blank_list or blank_list equal to 0
+while '_' in blank_list or len(blank_list) == 0:
+    
+    # Ask to the user a letter and convert in a lower letter
+    guess = input("Guess a letter: ").lower()
 
-position = 0
-for letter in chosen_word:
-    if letter == guess:
-        blank_list[position] = letter
-        position += 1
-    else:
-        position += 1
+    print(blank_list)
 
-print(blank_list)
+    # Modify the value of the blank list if the letter is in chosen_word
+    position = 0
+    for letter in chosen_word:
+        if letter == guess:
+            blank_list[position] = letter
+            position += 1
+        else:
+            position += 1
+
+    print(blank_list)
+
+print('You won')
