a4079bec0ddf13570f6ca56acc392e45beaedeab
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-10-19 22:00:59 -0500

    Se agrega lista en blanco con el total de caracteres de palabra seleccionada, se realiza la asignación de la posición de la letra a la lista en blanco

--- day-seven/hangman.py
+++ day-seven/hangman.py
@@ -9,6 +9,7 @@ word_list = ["jirafa","elefante","tigre","leon","pulpo","tiburon","dinosaurio"]
 # Step 2: Choose randomly a word into the list
 
 chosen_word = random.choice(word_list)
+print(f'This is a clue {chosen_word}')
 
 # Step 3: Ask to the user a letter
 
@@ -21,3 +22,21 @@ for letter in chosen_word:
         print("Right")
     else:
         print("Wrong")
+
+# Step 5: Create a blank list and replace the spaces '_' with the correct letter
+
+blank_list = []
+for i in range(len(chosen_word)):
+  blank_list.append("_")
+
+print(blank_list)
+
+position = 0
+for letter in chosen_word:
+    if letter == guess:
+        blank_list[position] = letter
+        position += 1
+    else:
+        position += 1
+
+print(blank_list)
