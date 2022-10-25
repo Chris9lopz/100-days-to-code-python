9f2fa0a1a8cbfb908397c9e5a161377a51dd27e4
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-10-24 21:09:44 -0500

    Se modifica ubicación de la declaración de la función y se ajusta bandera de continudad del programa

--- day-eight/caesar_cipher.py
+++ day-eight/caesar_cipher.py
@@ -6,15 +6,7 @@ print(logo) # Print logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-end_program = False
-
-while not end_program: 
-    # Inputs 
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-
-    def caesar(text,shift,direction): # Calling parameters
+def caesar(text,shift,direction): # Calling parameters
         empty_text = [] # Create a empty list
         for letter in text:
             if letter in alphabet:
@@ -33,10 +25,18 @@ while not end_program:
         else:
             print(f"The dencode text is {new_word}") # Output message default
 
+end_program = False # Flag to finish the program
+
+while not end_program: 
+    # Inputs 
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
     caesar(text=text, shift=shift, direction=direction) # Calling the function
 
     restart = input('Do you want to restart caesar cipher program? ').lower() # Ask if the user want to restart
     
-    if restart == 'no':
+    if restart == 'no': # If answer is no the program will restart
         print('Goodbye')
         end_program = True
