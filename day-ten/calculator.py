241c7687b9944ecf4e890889048d8c498499c33d
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-11-01 17:01:18 -0500

    Se agrega modulo art, se realiza programa final de calculadora y primer uso de recursividad

--- day-ten/calculator.py
+++ day-ten/calculator.py
@@ -1,3 +1,5 @@
+from art import logo
+
 # Adding
 def add(n1,n2):
     return n1 + n2
@@ -21,16 +23,36 @@ calculator = {
     '/' : divide,
 }
 
-num1 = int(input('What is the first number?: '))
+def calculating():
 
-for option in calculator:
-    print(option)
+    print(logo)
 
-symbol = input('Pick an operation from the line of above: ')
+    num1 = float(input('What is the first number?: '))
 
-num2 = int(input('What is the second number?: '))
+    for option in calculator:
+        print(option)
 
-operation_function = calculator[symbol]
-answer = operation_function(num1,num2)
+    end_program = True
 
-print(f'{num1} {symbol} {num2} = {answer}')
+    while end_program:
+
+        symbol = input('Pick an operation from the line of above: ')
+
+        num2 = float(input('What is the next number?: '))
+
+        operation_function = calculator[symbol]
+        answer = operation_function(num1,num2)
+
+        print(f'{num1} {symbol} {num2} = {answer}')
+
+        option = input(f'Type \'y\' to continue calculating with {answer}, or type \'n\' to start over. Use other key to exit: ' )
+
+        if option == 'y':
+            num1 = answer
+        elif option == 'n':
+            end_program = False
+            calculating()
+        else:
+            end_program = False
+
+calculating()
